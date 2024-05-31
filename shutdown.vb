bd54eafb9ebfa4eba7562545bd53f090aad7cc3e
JojoLaserjet <143895219+JojoLaserjet@users.noreply.github.com>
2024-05-31 13:34:18 +0200

    Update shutdown.vb

--- shutdown.vb
+++ shutdown.vb
@@ -1,11 +1,9 @@
 Set objShell = CreateObject("WScript.Shell")
 Set objFSO = CreateObject("Scripting.FileSystemObject")
 
-' Pfad zum Bild auf dem Desktop
 desktopPath = objShell.SpecialFolders("Desktop")
-imagePath = desktopPath & "\feierabend.png" ' Hier "feierabend.png" durch den Namen deines Bildes ersetzen
+imagePath = desktopPath & "C:\Users\admin\Downloads\shutdown-main\shutdown.png" '
 
-' Nachricht anzeigen, wenn das Bild auf dem Desktop geklickt wird
 If objFSO.FileExists(imagePath) Then
     result = objShell.Popup("Du hast jetzt viel gearbeitet! Jetzt ist Feierabend!", 0, "Feierabend", 4 + 32)
     If result = 6 Then ' Ja
